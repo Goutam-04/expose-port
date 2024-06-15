2ec14a0c756697acec7eb8705a52267270c47036
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-16 01:57:05 +0530

    completed the basic with wifi support only

--- bin/expose-port.js
+++ bin/expose-port.js
@@ -16,7 +16,10 @@ else if (options.wifi === true) console.log('add port to expose');
 else {
     const port=options.wifi;
     const address=`${getIPAddress()}:${port}`
-    console.log(address);
+    //i just asked ai to make this a clickable link , i dont understand wht this is ,
+    // most probaly i will replace this later as it is very late at night i dont have any much more patience now😅
+    const clickableLink = `\u001b]8;;http://${address}\u0007${address}\u001b]8;;\u0007`;
+    console.log(clickableLink);
     
     // console.log(`the port no is ${port} ...`);
     }
