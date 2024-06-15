3784e1d17e589e6388b2e02cc313b634e5704b81
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-16 01:41:29 +0530

    got the address for the wifi

--- bin/expose-port.js
+++ bin/expose-port.js
@@ -1,3 +1,22 @@
-module.exports = function() {
-    console.log('hello world');
-  };
+#!/usr/bin/env node
+
+import {program }from 'commander'
+import getIPAddress from '../lib/ipconfig.js'
+
+program
+  .option('-w, --wifi [port]', 'Add port');
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.wifi === undefined) console.log('add any kind of wifi');
+else if (options.wifi === true) console.log('add port to expose');
+
+
+else {
+    const port=options.wifi;
+    const address=`${getIPAddress()}:${port}`
+    console.log(address);
+    
+    // console.log(`the port no is ${port} ...`);
+    }
